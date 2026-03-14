509fae1e967ccbab0302b79b8c5db253dd91dc12
Zeeshan Akram <za3819486@gmail.com>
2026-03-14 16:40:14 +0500

    Add files via upload

---
+++ Lab1/Firstletter.cpp
@@ -0,0 +1,24 @@
+/***********************************
+	Lab:	04
+	Task:	01
+	Developer:
+		Roll #:	F25BBITAB020
+		Name:	Zainab		
+***********************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "*********"<<endl;
+	cout << "        *"<<endl;
+	cout << "       * "<<endl;
+	cout << "      *  "<<endl;
+	cout << "     *   "<<endl;
+	cout << "    *    "<<endl;
+	cout << "   *     "<<endl;
+	cout << "  *      "<<endl;
+	cout << " *       "<<endl;
+	cout << "*********"<<endl;
+	return 0;
+}
---
+++ Lab1/semester_result.cpp
@@ -0,0 +1,24 @@
+/***********************************
+	Lab:	04
+	Task:	02
+	Developer:
+		Roll #:	F25BBITAB020
+		Name:	Zainab		
+***********************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout <<"Subject                                  Total Marks     Obtained Marks     Grade" <<endl <<endl;
+	cout <<"Functional English                           100               70             B+ " <<endl;
+	cout <<"Pakistan Study                               100               72             B+ " <<endl;
+	cout <<"Quantative Reasoning 1                       100               80             A- " <<endl;
+	cout <<"Holy Quran Translation                       100               90             A  " <<endl;
+	cout <<"Introduction to Computing Technology         100               86             A  " <<endl;
+	cout <<"Introduction to Philosopy                    100               78             A- " <<endl;
+	cout <<"Principles of Management                     100               88             A  " <<endl <<endl;
+	cout <<"Status:     Pass" <<endl;
+	cout <<"GPA:        3.6 " <<endl;
+	cout <<"CGPA:       3.6 " <<endl;
+}
